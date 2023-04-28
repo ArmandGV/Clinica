922fa6ead3f5e1cd7cb0effdb4d068f112b271cd
armando <armand_app@outlook.com>
2023-04-27 19:58:28 -0600

    Creando la relacion con el modelos y controller de Medico y relacionando para la creacion de codigo SQL

---
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -0,0 +1,30 @@
+package med.voll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Direccion {
+	
+	private String calle;
+	private String numero;
+	private String complemento;
+	private String distrito;
+	private String ciudad;
+	public Direccion(DatosDireccion datosDireccion) {
+		super();
+		this.calle = datosDireccion.calle();
+		this.numero = datosDireccion.numero();
+		this.complemento = datosDireccion.complemento();
+		this.distrito = datosDireccion.distrito();
+		this.ciudad = datosDireccion.ciudad();
+	}
+	
+	
+
+}
