efbf738aac301045ef5d5c6386dfb0facad2cdd3
armando <armand_app@outlook.com>
2023-05-04 17:09:43 -0600

    Realizando metodo update en medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,17 +1,19 @@
 package med.voll.api.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosActualizarMedico;
 import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
@@ -30,9 +32,18 @@ public class MedicoController {
 	}
 	
 	@GetMapping
-	public Page<DatosListadoMedico> listadoMedicos(Pageable paginacion){
+	public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2)  Pageable paginacion){
 		
 		return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
+		
+	}
+	
+	@PutMapping 
+	@Transactional
+	public void actualizarMedico(@RequestBody @Valid DatosActualizarMedico datosActualizarMedico ) {
+		
+		Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+		medico.actualizarDatos(datosActualizarMedico);
 		
 	}
 
--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -27,6 +27,17 @@ public class Direccion {
 		this.distrito = direccion.distrito();
 		this.ciudad = direccion.ciudad();
 	}
+
+
+	public Direccion actualizarDatos(DatosDireccion direccion) {
+		this.calle = direccion.calle();
+		this.numero = direccion.numero();
+		this.complemento = direccion.complemento();
+		this.distrito = direccion.distrito();
+		this.ciudad = direccion.ciudad();
+		return this;
+	}
+	
 	
 
 }
