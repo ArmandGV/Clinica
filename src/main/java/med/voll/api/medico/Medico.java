fd3c7d0df3066b0414ac1a05fd7785ba3a05d704
armando <armand_app@outlook.com>
2023-05-05 12:43:44 -0600

    Refactorizando nuestros response

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -30,6 +30,7 @@ public class Medico {
 	private String email;
 	private String telefono;
 	private String documento;
+	private Boolean activo;
 	@Enumerated(EnumType.STRING)
 	private Especialidad especialidad;
 	@Embedded
@@ -38,7 +39,7 @@ public class Medico {
 	
 	public Medico(DatosRegistroMedico datosRegistroMedico) {
 		
-		
+		this.activo = true;
 		this.nombre = datosRegistroMedico.nombre();
 		this.email = datosRegistroMedico.email();
 		this.telefono = datosRegistroMedico.telefono();
@@ -60,6 +61,12 @@ public class Medico {
 		}
 	
 	
+	}
+
+
+	public void desactivarMedico() {
+		this.activo = false;
+		
 	}
 	
 	
