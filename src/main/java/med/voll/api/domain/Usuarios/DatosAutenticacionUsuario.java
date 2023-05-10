67507db760b9367f5151ace36f40a1ea26814911
armando <armand_app@outlook.com>
2023-05-09 19:36:12 -0600

    Implementando Autenticacion de usuario

---
+++ src/main/java/med/voll/api/domain/Usuarios/DatosAutenticacionUsuario.java
@@ -0,0 +1,5 @@
+package med.voll.api.domain.Usuarios;
+
+public record DatosAutenticacionUsuario(String login, String clave) {
+
+}
---
+++ src/main/resources/db/migration/V6__create-table-usuario.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuarios(
+
+	id bigint NOT NULL AUTO_INCREMENT,
+	login VARCHAR(100) NOT NULL,
+	clave VARCHAR(300) NOT NULL,
+
+	
+	PRIMARY KEY(id)
+	
+);
