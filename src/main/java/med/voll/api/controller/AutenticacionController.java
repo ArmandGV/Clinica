d6c762ea4733a4c447ad56a087f02f6c78c652d0
armando <armand_app@outlook.com>
2023-05-13 20:24:47 -0600

    Implementando token JWT

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.Usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.Usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTToken;
+import med.voll.api.infra.security.TokenService;
 
 
 
@@ -21,13 +24,18 @@ public class AutenticacionController {
 	
 	@Autowired
 	private AuthenticationManager  authenticationManager;
+	
+	@Autowired
+	private TokenService tokenService;
 
 	@PostMapping
 	public ResponseEntity AutenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario) {
 		
-		Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
-		authenticationManager.authenticate(token);
-		return ResponseEntity.ok().build();
+		Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), 
+				datosAutenticacionUsuario.clave());
+		var usuarioAutenticado = authenticationManager.authenticate(authToken);
+		var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+		return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
 		
 	}
 
